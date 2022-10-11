f045811cf034df1d47c4c842eb39dc1e47fdb374
Zhizhuo Zhang <73738815+ZhizhuoUp@users.noreply.github.com>
2022-10-11 19:29:13 -0400

    Update train.py

--- train.py
+++ train.py
@@ -11,53 +11,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """
-    Callback for saving a model (the check is done every ``check_freq`` steps)
-    based on the training reward (in practice, we recommend using ``EvalCallback``).
-
-    :param check_freq: (int)
-    :param log_dir: (str) Path to the folder where the model will be saved.
-      It must contains the file created by the ``Monitor`` wrapper.
-    :param verbose: (int)
-    """
-
-    def __init__(self, check_freq: int, log_dir: str, verbose=1):
-        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, 'best_model')
-        self.best_mean_reward = -np.inf
-
-    def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-
-            # Retrieve training reward
-            x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-            if len(x) > 0:
-                # Mean training reward over the last 100 episodes
-                mean_reward = np.mean(y[-100:])
-                if self.verbose > 0:
-                    print("Num timesteps: {}".format(self.num_timesteps))
-                    print(
-                        "Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward,
-                                                                                                 mean_reward))
-
-                # New best model, you could save the agent here
-                if mean_reward > self.best_mean_reward:
-                    self.best_mean_reward = mean_reward
-                    # Example for saving best model
-                    if self.verbose > 0:
-                        print("Saving new best model to {}".format(self.save_path))
-                    self.model.save(self.save_path)
-
-        return True
-
 def run():
     torch.multiprocessing.freeze_support()
     print('loop')
@@ -84,7 +37,7 @@ if __name__ == '__main__':
 
         mean_reward_list = []
         std_reward_list = []
-        N_ID = 2
+        N_ID = 1
         model_save_path = log_dir+"run%d"%N_ID
         try:
             os.mkdir(model_save_path)
@@ -109,16 +62,21 @@ if __name__ == '__main__':
         print('finished')
 
     if mode == 1:
+        
         N_ID = 0
         log_path= "models/trainrun%d/"%N_ID
-        env = Arm_env(max_step = MAX_STEP, is_render=True)
+        env = Arm_env(max_step = MAX_STEP * 5, is_render=False)
 
         model = PPO.load(log_path + "best_model", env)
         obs = env.reset()
         for i in range(10000):
 
             a, _ = model.predict(obs)
+            print(f'action {a}')
             obs, reward, done, _ = env.step(a)
+            # print(f'ee_pos {obs[:3]}')
+            # print(f'box_pos {obs[6:9]}')
+            print(reward)
 
             if done == True:
                 print("fail")
